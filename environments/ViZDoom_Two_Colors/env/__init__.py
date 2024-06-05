04b2f4649324c680025a9870f2b0f2815a32fc72
Avanturist322 <cherepanovegor2018@gmail.com>
2024-06-05 15:31:41 +0300

    raw vizdoom commit

---
+++ environments/ViZDoom_Two_Colors/env/__init__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+parent_dir = os.path.dirname(parent_dir)
+sys.path.append(parent_dir)
+
+# from .get_vizdoom_dataset import *
+# from ..env import *
+from .common_wrappers import *
+from .env_vizdoom import *
+# from .normalization import *
+# from .get_vizdoom_iter_dataset import *
